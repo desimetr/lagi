401707b1522389c4939edb7dd33b237888647e98
desimetr <51058335+desimetr@users.noreply.github.com>
2024-09-03 15:22:04 +0330

    Update app.js

--- app.js
+++ app.js
@@ -19,3 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
